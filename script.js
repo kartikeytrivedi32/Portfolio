c9b7dc0f2138c9e08a6dbc64cf88541db6a2d108
Kartikey Trivedi <kartiktrivedikartik@gmail.com>
2025-07-06 10:48:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for nav links
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
